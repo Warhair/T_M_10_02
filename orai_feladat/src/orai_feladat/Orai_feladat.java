98eb2db60ba2b2a1faf97ed3a04aedbc33681153
Warhair <tegzesmarton@gmail.com>
2023-10-02 09:41:59 +0200

    utolso modositas

--- orai_feladat/src/orai_feladat/Orai_feladat.java
+++ orai_feladat/src/orai_feladat/Orai_feladat.java
@@ -1,9 +1,11 @@
 
 package orai_feladat;
 
+import java.util.Scanner;
+
 
 public class Orai_feladat {
-    
+   
     public static String[] pakli = {"0","Z_1","Z_2","Z_3","Z_4","Z_5","Z_6","M_1","M_2","M_3","M_4","M_5","M_6","P_1","P_2","P_3","P_4","P_5","P_6","T_1","T_2","T_3"};
     
     public static void main(String[] args) {
@@ -36,35 +38,45 @@ public class Orai_feladat {
         switch(melyik()){
             case 1:{
                 for (int i = 1; i < pakli.length; i++) {
-                pakli[1-7] = pakli[20-(i-1)*3];
-                pakli[7-14] = pakli[19-(i-1)*3];
-                pakli[14-21] = pakli[21-(i-1)*3];
+                if(i <= 7){
+                    pakli[i] = pakli[20-(i-1)*3];
+                }
+                
                 }
             }
              case 2:{
                 for (int i = 1; i < pakli.length; i++) {
-                pakli[1-7] = pakli[19-(i-1)*3];
-                pakli[7-14] = pakli[20-(i-1)*3];
-                pakli[14-21] = pakli[21-(i-1)*3];
+               if(i <= 7){
+                    pakli[i] = pakli[20-(i-1)*3];
+                }
+                
                 }
             } case 3:{
                 for (int i = 1; i < pakli.length; i++) {
-                pakli[1-7] = pakli[21-(i-1)*3];
-                pakli[7-14] = pakli[19-(i-1)*3];
-                pakli[14-21] = pakli[20-(i-1)*3];
+                if(i <= 7){
+                    pakli[i] = pakli[20-(i-1)*3];
+                }
+               
                 }
             }
              
         }
     }
 
-    private static int melyik() {
+    private static int melyik() { 
+        int oszlop  = 1;
+        do{
+            System.out.println("Melyik oszlopot választod");
+        }
+        while(oszlop <= 1 && oszlop >= 3);
+        return oszlop;
         
     }
 
     private static void ezvolt() {
         System.out.println("Ez volt a válsaztott elem"+pakli[11]);
     }
+    
   
     
     
