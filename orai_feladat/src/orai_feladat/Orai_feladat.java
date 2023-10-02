172c020bfedcb31b78715745eaba9c9ab23dc659
Warhair <tegzesmarton@gmail.com>
2023-10-02 09:12:33 +0200

    kiir_megirva

--- orai_feladat/src/orai_feladat/Orai_feladat.java
+++ orai_feladat/src/orai_feladat/Orai_feladat.java
@@ -1,20 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package orai_feladat;
 
-/**
- *
- * @author tegzes.marton
- */
-public class Orai_feladat {
 
-    /**
-     * @param args the command line arguments
-     */
+public class Orai_feladat {
+    
+    public static String[] Pakli = {"0","Z_1","Z_2","Z_3","Z_4","Z_5","Z_6","M_1","M_2","M_3","M_4","M_5","M_6","P_1","P_2","P_3","P_4","P_5","P_6","T_1","T_2","T_3"};
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        jatek();
     }
+
+    private static void jatek() {
+        kiir();
+        for (int i = 0; i < 3; i++) {
+            
+        }
+    }
+
+    private static void kiir() {
+        for (int i = 1; i < Pakli.length; i++) {
+            System.out.printf(Pakli[i]+" "); 
+            if(i % 3 == 0){
+                System.out.println();
+                
+            }
+            
+            
+        }
+        
+    }
+  
+    
     
 }
